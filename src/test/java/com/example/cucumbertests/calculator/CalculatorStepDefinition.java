dfa9c92c5a408c7653c4bd9cd7f9b4e8be504cdf
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-18 19:04:17 +0530

    test: Create object of Calculator class and assign it in Given.

--- src/test/java/com/example/cucumbertests/calculator/CalculatorStepDefinition.java
+++ src/test/java/com/example/cucumbertests/calculator/CalculatorStepDefinition.java
@@ -3,8 +3,10 @@ package com.example.cucumbertests.calculator;
 import io.cucumber.java.en.Given;
 
 public class CalculatorStepDefinition {
-    
+    private Calculator calculator;
+
     @Given("I have a calculator")
     public void iHaveACalculator() {
+        calculator = new Calculator();
     }
 }
