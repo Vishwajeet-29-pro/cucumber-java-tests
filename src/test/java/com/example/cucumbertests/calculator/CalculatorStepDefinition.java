ae4db150846ac6113e32c2677849fb0ca36613b0
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-18 19:08:13 +0530

    test: Add step definition for When.

--- src/test/java/com/example/cucumbertests/calculator/CalculatorStepDefinition.java
+++ src/test/java/com/example/cucumbertests/calculator/CalculatorStepDefinition.java
@@ -1,12 +1,19 @@
 package com.example.cucumbertests.calculator;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class CalculatorStepDefinition {
     private Calculator calculator;
+    private int result;
 
     @Given("I have a calculator")
     public void iHaveACalculator() {
         calculator = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void iAddAnd(int num1, int num2) {
+        result = calculator.add(num1, num2);
     }
 }
