4e1add78652535b4ba510bfd8212687e19a00506
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-18 19:01:26 +0530

    test: Create step definition class and implement Given.

---
+++ src/test/java/com/example/cucumbertests/calculator/CalculatorStepDefinition.java
@@ -0,0 +1,10 @@
+package com.example.cucumbertests.calculator;
+
+import io.cucumber.java.en.Given;
+
+public class CalculatorStepDefinition {
+    
+    @Given("I have a calculator")
+    public void iHaveACalculator() {
+    }
+}
