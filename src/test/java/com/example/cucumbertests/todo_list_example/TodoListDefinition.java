bf1ed5996eb51ea63238ffa9b2e0094cca5b7c62
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-19 22:22:48 +0530

    test: Create an object of Task class as List of and initialize object with ArrayList.

--- src/test/java/com/example/cucumbertests/todo_list_example/TodoListDefinition.java
+++ src/test/java/com/example/cucumbertests/todo_list_example/TodoListDefinition.java
@@ -1,9 +1,17 @@
 package com.example.cucumbertests.todo_list_example;
 
 import io.cucumber.java.en.Given;
+import todo.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoListDefinition {
+    private List<Task> todoList;
+
     @Given("I have an empty to-do list")
     public void iHaveAnEmptyToDoList() {
+        todoList = new ArrayList<>();
     }
+
 }
