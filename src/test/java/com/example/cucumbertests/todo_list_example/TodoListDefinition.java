b6d17c2a7ba0f2af7cd2db4601d57d16393455b9
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-19 22:15:00 +0530

    test: Create test class for step definition and Create Given.

---
+++ src/test/java/com/example/cucumbertests/todo_list_example/TodoListDefinition.java
@@ -0,0 +1,9 @@
+package com.example.cucumbertests.todo_list_example;
+
+import io.cucumber.java.en.Given;
+
+public class TodoListDefinition {
+    @Given("I have an empty to-do list")
+    public void iHaveAnEmptyToDoList() {
+    }
+}
