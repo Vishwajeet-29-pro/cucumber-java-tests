e4b1d54e3537cd7711935725c66d557f82ee4d9b
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-19 22:20:43 +0530

    feat: Add data class for the todo list.

---
+++ src/main/java/todo/Task.java
@@ -0,0 +1,11 @@
+package todo;
+
+public class Task {
+   private String name;
+   private String priority;
+
+    public Task(String name, String priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+}
